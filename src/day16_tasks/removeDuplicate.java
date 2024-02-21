4ab12f97e04e85ffc6fd2a3565e9ccc5ed0b5853
aissam0331 <155390831+aissam0331@users.noreply.github.com>
2024-02-20 19:15:10 -0500

    java self practice task 1

---
+++ src/day16_tasks/removeDuplicate.java
@@ -0,0 +1,21 @@
+package day16_tasks;
+
+import java.util.*;
+
+public class removeDuplicate {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<> (  );
+        list.addAll (Arrays.asList (20, 15, 10, 20, 30, 10, 20, 15, 0));
+        System.out.println (list );
+        TreeSet<Integer> set = new TreeSet<> (  );
+        set.addAll (Arrays.asList (20, 15, 10, 20, 30, 10, 20, 15, 0));
+        System.out.println (set );
+
+
+
+
+
+
+    }
+
+}
